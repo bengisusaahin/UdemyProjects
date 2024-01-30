1292bb5d6cab899ff147df11482fe607efb69a89
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-01-30 17:00:04 +0300

    Menu added for the add art

--- Project01/app/src/main/java/com/bengisusahin/artbookwithfragment/MainActivity.java
+++ Project01/app/src/main/java/com/bengisusahin/artbookwithfragment/MainActivity.java
@@ -1,8 +1,15 @@
 package com.bengisusahin.artbookwithfragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_art_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_art_menu){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
